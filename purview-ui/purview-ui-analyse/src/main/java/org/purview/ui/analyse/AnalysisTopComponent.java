add9dbdc577bc6d2408b8e1f4f97798ca6df576d
David Flemström <david.flemstrom@gmail.com>
2010-03-02 00:53:56 +0100

    Added settings management to the GUI

--- purview-ui/purview-ui-analyse/src/main/java/org/purview/ui/analyse/AnalysisTopComponent.java
+++ purview-ui/purview-ui-analyse/src/main/java/org/purview/ui/analyse/AnalysisTopComponent.java
@@ -4,28 +4,26 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.util.LinkedList;
-import javax.swing.DefaultListModel;
+import java.util.List;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import org.openide.awt.Mnemonics;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
+import org.openide.windows.Mode;
 import org.openide.windows.TopComponent;
-import org.purview.analysers.DefaultAnalysers;
 import org.purview.core.analysis.Analyser;
 import org.purview.core.data.Color;
 import org.purview.core.data.Matrix;
 import org.purview.core.data.MatrixOps;
 import org.purview.core.session.AnalysisSession;
 import org.purview.core.session.AnalysisStats;
-import scala.Function0;
-import scala.collection.Iterator;
 import scala.collection.JavaConversions;
-import scala.collection.Seq;
 
 /**
  * Top component which displays something.
@@ -33,7 +31,7 @@ import scala.collection.Seq;
 final class AnalysisTopComponent extends TopComponent implements Runnable {
 
     private static final String ICON_PATH = "org/purview/ui/analyse/analyse.png";
-    private static final String PREFERRED_ID = "AnalysisComponent";
+    private static final String PREFERRED_ID = "AnalysisTopComponent";
     private final JLabel analyserDescrLabel = new JLabel();
     private final JLabel analyserLabel = new JLabel();
     private final JScrollPane listScroller = new JScrollPane();
@@ -41,51 +39,46 @@ final class AnalysisTopComponent extends TopComponent implements Runnable {
     private final JProgressBar progressBar = new JProgressBar();
     private final JLabel stageDescrLabel = new JLabel();
     private final JLabel stageLabel = new JLabel();
-    private final DefaultListModel model = new DefaultListModel();
-    private final JList statusList = new JList();
+    private final JTextArea statusList = new JTextArea();
     private final AnalysisSession session;
 
-    public AnalysisTopComponent(final String imageName, final BufferedImage image) {
+    public AnalysisTopComponent(final String imageName, final BufferedImage image,
+            final List<Analyser<Matrix<Color>>> analysers) {
         initComponents();
         setName(NbBundle.getMessage(AnalysisTopComponent.class, "CTL_AnalysisTopComponent", imageName));
         setToolTipText(NbBundle.getMessage(AnalysisTopComponent.class, "HINT_AnalysisTopComponent"));
         setIcon(ImageUtilities.loadImage(ICON_PATH, true));
 
-        //Do some really nifty Scala interop conversions!
-        Seq<Function0<Analyser<Matrix<Color>>>> analysers = DefaultAnalysers.analysers();
-        Iterator<Function0<Analyser<Matrix<Color>>>> iter = analysers.iterator();
-        LinkedList<Analyser<Matrix<Color>>> result = new LinkedList<Analyser<Matrix<Color>>>();
-
-        while (iter.hasNext()) {
-            Function0<Analyser<Matrix<Color>>> c = iter.next();
-            result.add(c.apply());
-        }
-
+        Matrix<Color> matrix = MatrixOps.imageToMatrix(image);
         session = new AnalysisSession<Matrix<Color>>(
-                JavaConversions.asBuffer(result).toSeq(),
-                MatrixOps.imageToMatrix(image));
+                JavaConversions.asBuffer(analysers).toSeq(),
+                matrix);
     }
 
     public void run() {
         AnalysisStats stats = new AnalysisStats() {
+            private int oldProgress = 0;
 
             @Override
             public void reportProgress(final float progress) {
-                SwingUtilities.invokeLater(new Runnable() {
+                final int actualProgress = (int) (1000 * progress);
+                if(actualProgress > oldProgress) {
+                    SwingUtilities.invokeLater(new Runnable() {
 
-                    public void run() {
-                        progressBar.setValue((int) (10000 * progress));
-                    }
-                });
+                        public void run() {
+                            progressBar.setValue(actualProgress);
+                        }
+                    });
+                    oldProgress = actualProgress;
+                }
             }
 
             @Override
             public void reportStatus(final String status) {
-                model.addElement(status);
                 SwingUtilities.invokeLater(new Runnable() {
 
                     public void run() {
-                        statusList.repaint();
+                        statusList.append(status + '\n');
                     }
                 });
             }
@@ -110,7 +103,7 @@ final class AnalysisTopComponent extends TopComponent implements Runnable {
                 });
             }
         };
-        session.run(stats);
+        System.out.println(session.run(stats));
     }
 
     /**
@@ -120,22 +113,35 @@ final class AnalysisTopComponent extends TopComponent implements Runnable {
     private void initComponents() {
 
         setLayout(new BorderLayout());
+
+        progressBar.setMinimum(0);
+        progressBar.setMaximum(1000);
+
         add(progressBar, BorderLayout.PAGE_END);
 
-        mainPanel.setLayout(new GridLayout(2, 2));
+        mainPanel.setLayout(new GridLayout(2, 1));
 
-        Mnemonics.setLocalizedText(analyserDescrLabel, NbBundle.getMessage(AnalysisTopComponent.class, "LBL_Analyser"));
-        mainPanel.add(analyserDescrLabel);
+        Mnemonics.setLocalizedText(analyserDescrLabel, NbBundle.getMessage(AnalysisTopComponent.class, "LBL_Analyser") + ": ");
 
-        mainPanel.add(analyserLabel);
+        JPanel container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.LINE_AXIS));
+        container.add(analyserDescrLabel);
+        container.add(analyserLabel);
 
-        Mnemonics.setLocalizedText(stageDescrLabel, NbBundle.getMessage(AnalysisTopComponent.class, "LBL_Stage"));
-        mainPanel.add(stageDescrLabel);
+        mainPanel.add(container);
 
-        mainPanel.add(stageLabel);
+        Mnemonics.setLocalizedText(stageDescrLabel, NbBundle.getMessage(AnalysisTopComponent.class, "LBL_Stage") + ": ");
+
+        container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.LINE_AXIS));
+        container.add(stageDescrLabel);
+        container.add(stageLabel);
+
+        mainPanel.add(container);
 
         add(mainPanel, BorderLayout.PAGE_START);
 
+        statusList.setEditable(false);
         listScroller.setViewportView(statusList);
 
         add(listScroller, BorderLayout.CENTER);
@@ -149,5 +155,15 @@ final class AnalysisTopComponent extends TopComponent implements Runnable {
     @Override
     protected String preferredID() {
         return PREFERRED_ID;
+    }
+
+    @Override
+    public List<Mode> availableModes(List<Mode> input) {
+        LinkedList<Mode> result = new LinkedList<Mode>();
+        for(Mode mode : input) {
+            if(mode.getName().equals("output") || mode.getName().endsWith("NewMode"))
+                result.add(mode);
+        }
+        return result;
     }
 }
--- purview-ui/purview-ui-analyse/src/main/java/org/purview/ui/analyse/ImageSessionTopComponent.java
+++ purview-ui/purview-ui-analyse/src/main/java/org/purview/ui/analyse/ImageSessionTopComponent.java
@@ -1,12 +1,17 @@
 package org.purview.ui.analyse;
 
 import java.awt.BorderLayout;
+import java.awt.Frame;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -17,6 +22,14 @@ import org.openide.awt.Mnemonics;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
+import org.purview.analysers.DefaultAnalysers;
+import org.purview.core.analysis.Analyser;
+import org.purview.core.data.Color;
+import org.purview.core.data.Matrix;
+import scala.Function0;
+import scala.collection.Iterator;
+import scala.collection.Seq;
 
 /**
  * Top component which displays something.
@@ -24,27 +37,46 @@ import org.openide.windows.TopComponent;
 final class ImageSessionTopComponent extends TopComponent implements ActionListener {
 
     private static final String ICON_PATH = "org/purview/ui/analyse/gears.png";
-    private static final String PREFERRED_ID = "AnalyserSession";
+    private static final String PREFERRED_ID = "ImageSessionTopComponent";
     private final JButton analyseButton;
+    private final JButton configureAnalysersButton;
     private final JScrollPane scrollPane;
     private final JToolBar toolbar;
     private final BufferedImage image;
     private final String name;
+    private final Map<Analyser<Matrix<Color>>, Boolean> analysers;
 
     ImageSessionTopComponent(final File imageFile) throws IOException {
+        //Load the image
         BufferedImage img = ImageIO.read(imageFile);
 
+        //Convert image to the internal ARGB format
         BufferedImage argbVersion =
                 new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = argbVersion.createGraphics();
         g.drawImage(img, 0, 0, null);
         g.dispose();
 
+        //Store the image
         image = argbVersion;
         name = imageFile.getName();
 
+        //Do some really nifty Scala interop conversions to load default analysers
+        Seq<Function0<Analyser<Matrix<Color>>>> analyserFactories = DefaultAnalysers.analysers();
+        Iterator<Function0<Analyser<Matrix<Color>>>> iter = analyserFactories.iterator();
+        HashMap<Analyser<Matrix<Color>>, Boolean> createdAnalysers =
+                new HashMap<Analyser<Matrix<Color>>, Boolean>();
+
+        while (iter.hasNext()) {
+            Function0<Analyser<Matrix<Color>>> c = iter.next();
+            createdAnalysers.put(c.apply(), Boolean.TRUE);
+        }
+        analysers = createdAnalysers;
+
+        //Create GUI
         toolbar = new JToolBar();
         analyseButton = new JButton();
+        configureAnalysersButton = new JButton();
         scrollPane = new CheckerboardScrollPane(new ImagePanel(image));
 
         setLayout(new BorderLayout());
@@ -60,6 +92,16 @@ final class ImageSessionTopComponent extends TopComponent implements ActionListe
         analyseButton.setVerticalTextPosition(SwingConstants.BOTTOM);
         analyseButton.addActionListener(this);
         toolbar.add(analyseButton);
+
+        configureAnalysersButton.setIcon(
+                new ImageIcon(getClass().getResource("/org/purview/ui/analyse/settings.png")));
+        Mnemonics.setLocalizedText(configureAnalysersButton,
+                NbBundle.getMessage(ImageSessionTopComponent.class, "LBL_ConfigureAnalysers"));
+        configureAnalysersButton.setFocusable(false);
+        configureAnalysersButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        configureAnalysersButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        configureAnalysersButton.addActionListener(this);
+        toolbar.add(configureAnalysersButton);
 
         add(toolbar, BorderLayout.PAGE_START);
         add(scrollPane, BorderLayout.CENTER);
@@ -83,13 +125,22 @@ final class ImageSessionTopComponent extends TopComponent implements ActionListe
 
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == analyseButton) {
-            AnalysisTopComponent c = new AnalysisTopComponent(name, image);
+            LinkedList<Analyser<Matrix<Color>>> activeAnalysers = new LinkedList<Analyser<Matrix<Color>>>();
+            for(Analyser<Matrix<Color>> key : analysers.keySet()) {
+                if(analysers.get(key))
+                    activeAnalysers.add(key);
+            }
+            AnalysisTopComponent c = new AnalysisTopComponent(name, image, activeAnalysers);
             c.open();
             c.requestActive();
 
             Thread t = new Thread(c);
             t.setName("Analyser thread for image \"" + name + "\"");
             t.start();
+        } else if(e.getSource() == configureAnalysersButton) {
+            Frame window = WindowManager.getDefault().getMainWindow();
+            AnalyserSettingsDialog dialog = new AnalyserSettingsDialog(analysers, window, true);
+            dialog.setVisible(true);
         }
     }
 }
