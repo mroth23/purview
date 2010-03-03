75e3b3c4e2b88a8c7136594bc7c03917d59e4588
David Flemström <david.flemstrom@gmail.com>
2010-03-03 02:16:36 +0100

    Restructured ReportLevels some more

--- purview-core/src/main/scala/org/purview/core/report/ReportLevel.scala
+++ purview-core/src/main/scala/org/purview/core/report/ReportLevel.scala
@@ -3,33 +3,40 @@ package org.purview.core.report
 import org.purview.core.data.Color
 import org.purview.core.data.Colors
 
-class ReportLevel(val name: String) extends NotNull
+trait ReportLevel extends NotNull {
+  val name: String
+}
 
 trait LevelColor extends ReportLevel {
   def color: Color
 }
   
-object Debug extends ReportLevel("Debug")
+object Debug extends ReportLevel
                 with LevelColor {
+  val name = "Debug"
   val color = Color(1, 0, 0.5f, 0)
 }
 
-object Information extends ReportLevel("Information")
-                      with LevelColor  {
+object Information extends ReportLevel
+                      with LevelColor {
+  val name = "Information"
   val color = Colors.Green
 }
 
-object Warning extends ReportLevel("Warning")
+object Warning extends ReportLevel
                   with LevelColor {
+  val name = "Warning"
   val color = Colors.Yellow
 }
 
-object Error extends ReportLevel("Error")
+object Error extends ReportLevel
                 with LevelColor {
+  val name = "Error"
   val color = Colors.Red
 }
 
-object Critical extends ReportLevel("Critical")
+object Critical extends ReportLevel
                    with LevelColor {
+  val name = "Critical"
   val color = Color(1, 0, 0, 0)
 }
