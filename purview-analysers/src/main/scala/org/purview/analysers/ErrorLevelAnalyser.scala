6a913b883b4c6e3d93e6a6f0694188b4d97dbcbc
David Flemström <david.flemstrom@gmail.com>
2010-03-14 00:56:01 +0100

    Added ErrorLevelAnalyser

---
+++ purview-analysers/src/main/scala/org/purview/analysers/ErrorLevelAnalyser.scala
@@ -0,0 +1,73 @@
+package org.purview.analysers
+
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import javax.imageio.IIOImage
+import javax.imageio.ImageIO
+import javax.imageio.ImageWriteParam
+import org.purview.core.analysis.HeatMapImageAnalyser
+import org.purview.core.analysis.Metadata
+import org.purview.core.analysis.Settings
+import org.purview.core.analysis.settings.FloatRangeSetting
+import org.purview.core.report.Warning
+import org.purview.core.transforms.ImageToMatrix
+import org.purview.core.transforms.LinearConvolve
+import org.purview.core.transforms.MatrixToImage
+import scala.math._
+
+class ErrorLevelAnalyser extends HeatMapImageAnalyser with Settings with Metadata {
+  val name = "Error level analyser"
+  val description = "Uses compression errors in JPEG images to find errors"
+
+  val qualitySetting = new FloatRangeSetting("Quality level", 0, 1, 100)
+  qualitySetting.value = 0.2f //default
+  val thresholdSetting = new FloatRangeSetting("Threshold", 0, 1, 100)
+  thresholdSetting.value = 0.1f //default
+
+  private def quality: Float = qualitySetting.value
+  private def artifactThreshold: Float = thresholdSetting.value
+
+  val settings = List(qualitySetting, thresholdSetting)
+
+  override val message = "Noticeable compression artifact"
+  override val reportLevel = Warning
+  override val threshold = 0f //We implement our own threshold and don't want upstream interference
+  override val maxDeviationTolerance = 0.5f
+
+  private def introduceJPEGArtifacts(in: BufferedImage) = {
+    status("Generating JPEG artifacts")
+    val writers = ImageIO.getImageWritersByFormatName("jpeg")
+
+    val out = new ByteArrayOutputStream
+    val writer = writers.next()
+    try {
+      writer.setOutput(ImageIO.createImageOutputStream(out))
+
+      val param = writer.getDefaultWriteParam
+      param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT)
+      param.setCompressionQuality(quality)
+
+      writer.write(null, new IIOImage(in, null, null), param)
+
+      ImageIO.read(new ByteArrayInputStream(out.toByteArray))
+    } finally {
+      writer.dispose()
+      out.close()
+    }
+  }
+  
+  private val gaussian30Kernel =
+    (for(i <- -30 to 30) yield (30 - abs(i)) / (30f * 30f * 30f)).toArray
+  
+  private def errorMatrix = input >- MatrixToImage() >- introduceJPEGArtifacts >- ImageToMatrix()
+  
+  def diff = for(in <- input; artifacts <- errorMatrix) yield
+    in.zip(artifacts).map(x => {
+        val w = (x._1 - x._2).weight / 2 //2 because of sqrt(4); we want to average the color
+        if(w > artifactThreshold) w else 0
+      })
+
+  //Extreme blur!
+  def heatmap = diff >- {x => status("Convolving the result using a linear distribution"); x } >- LinearConvolve(gaussian30Kernel)
+}
