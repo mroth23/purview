52f5756e2f6d95845f67e616aee8f856bc930f45
David Flemström <david.flemstrom@gmail.com>
2010-03-14 20:21:33 +0100

    Added a metadata analyser that searches for basic identifying metadata tags

--- purview-analysers/src/main/scala/org/purview/analysers/DefaultAnalysers.scala
+++ purview-analysers/src/main/scala/org/purview/analysers/DefaultAnalysers.scala
@@ -4,6 +4,8 @@ import org.purview.core.analysis.Analyser
 import org.purview.core.data.ImageMatrix
 
 object DefaultAnalysers {
-    List(() => new Bilinear, () => new ExampleAnalyser, () => new ErrorLevelAnalyser, () => new LuminanceGradient)
   val analysers: Seq[() => Analyser[ImageMatrix]] =
+    List(() => new Bilinear, () => new ExampleAnalyser,
+         () => new ErrorLevelAnalyser, () => new LuminanceGradient,
+         () => new MetadataAnalyser)
 }
