232cd9a3dddd985655475572238167d3c5979e44
Moritz <moritz.gemini92@telia.com>
2010-04-08 12:30:43 +0200

    Improved luminance gradient

--- purview-analysers/luminance-gradient-analyser/src/main/scala/org/purview/luminancegradientanalyser/AnalyserImplementation.scala
+++ purview-analysers/luminance-gradient-analyser/src/main/scala/org/purview/luminancegradientanalyser/AnalyserImplementation.scala
@@ -13,26 +13,14 @@ import org.purview.core.report.ReportImage
 import org.purview.core.transforms.MatrixToImage
 import scala.math._
 
-class AnalyserImplementation extends Analyser[ImageMatrix] with Settings {
+class AnalyserImplementation extends Analyser[ImageMatrix] {
   val name = "Luminance Gradient"
   val description = "Plots the general light direction in the image"
-
-  val scaleSetting = FloatRangeSetting("Blue scale", 2, 20)
-  scaleSetting.value = 10 //default
-
-  val blendSetting = FloatRangeSetting("Blending", 0f, 1f)
-  blendSetting.value = 0f
-
-  val settings = List(scaleSetting, blendSetting)
-
-  private def scale = scaleSetting.value
-  private def blending = blendSetting.value
 
   val luminanceGradient = for(in <- input) yield {
     status("Calculating the luminance gradient for the image")
     for {
       (x, y, src) <- in.cells
-      srcBrightness = sqrt(pow(src.r, 2) *  0.241 + pow(src.g, 2) *  0.691 + pow(src.b, 2) *  0.068)
     } yield if(x == 0 || x == in.width - 1 || y == 0 || y == in.height - 1)
       Color.Black
     else
@@ -43,41 +31,45 @@ class AnalyserImplementation extends Analyser[ImageMatrix] with Settings {
         current = in(dx, dy)
         if !(x == 0 && y == 0)
       } yield (abs(dx-x) + abs(dy-y) match {
-          case 1 => ValueColorCell(sqrt(pow(current.r, 2) *  0.241 +
-                                        pow(current.g, 2) *  0.691 +
-                                        pow(current.b, 2) *  0.068) * 0.9712, current, dx, dy)
-          case 2 => ValueColorCell(sqrt(pow(current.r, 2) *  0.241 +
-                                        pow(current.g, 2) *  0.691 +
-                                        pow(current.b, 2) *  0.068) * 0.5445, current, dx, dy)
+          case 1 => ValueColorCell(calculateBrightness(current) * 0.9712, current, dx, dy)
+          case 2 => ValueColorCell(calculateBrightness(current) * 0.5445, current, dx, dy)
           case _ => ValueColorCell(0d, current, dx, dy)
         })
 
       val max = pixels.max
-      val diff = sqrt(pow(src.r - max.color.r, 2) +
-                      pow(src.g - max.color.g, 2) +
-                      pow(src.b - max.color.b, 2))
+	val min = pixels.min
 
-      val clr1 = x - max.x match {
-        case -1 => new Color(1, 0, 0, 0)
-        case 0 => new Color(1, 0, 0.5f, 0)
-        case 1 => new Color(1, 0, 1, 0f)
+      val diff = 0.2 + sqrt(pow(min.color.r - max.color.r, 2) +
+                      pow(min.color.g - max.color.g, 2) +
+                      pow(min.color.b - max.color.b, 2))
+
+      val gRes = max.x - min.x match {
+	  case -2 => 1.00f
+        case -1 => 0.75f
+        case 0  => 0.50f
+        case 1  => 0.25f
+	  case 2  => 0.00f
       }
 
-      val clr2 = y - max.y match {
-        case -1 => new Color(1f, 0f, clr1.g, 0f)
-        case 0 => new Color(1f, 0.5f, clr1.g, 0f)
-        case 1 => new Color(1f, 1f, clr1.g, 0f)
+      val rRes = max.y - min.y match {
+	  case -2 => 1.00f
+        case -1 => 0.75f
+        case 0  => 0.50f
+        case 1  => 0.25f
+	  case 2  => 0.00f
       }
 
-      new Color(1,
-                (1 - blending) * clr2.r               + blending * src.r,
-                (1 - blending) * clr2.g               + blending * src.g,
-                (1 - blending) * diff.toFloat * scale + blending * src.b)
+      new Color(1, rRes, gRes, diff.toFloat)
     }
   }
 
   def imageReport(img: BufferedImage): Set[ReportEntry] =
     Set(new ReportImage(Information, "Output image", 0, 0, img))
+
+  def calculateBrightness(pixel: Color) : Float = {
+    //sRGB luminance
+    (0.2126f * pixel.r + 0.7152f * pixel.g + 0.0722f * pixel.b)
+  }
 
   val result = luminanceGradient >- MatrixToImage() >- imageReport
 }
