90f2bdfcc526599173eabcd701ae550a3df5e7f2
David Flemström <david.flemstrom@gmail.com>
2010-05-09 15:37:54 +0200

    Fixed all of the issues that the JPEG ghost filter had

--- purview-analysers/jpeg-ghost-analyser/src/main/scala/org/purview/jpegghostanalyser/AnalyserImplementation.scala
+++ purview-analysers/jpeg-ghost-analyser/src/main/scala/org/purview/jpegghostanalyser/AnalyserImplementation.scala
@@ -17,20 +17,20 @@ class AnalyserImplementation extends Analyser[ImageMatrix] with Settings {
   val name = "JPEG Ghost Analyser"
   val description = "Finds JPEG ghosts in an image"
 
-  val minQualitySetting = IntRangeSetting("Minimum quality", 1, 100)
-  minQualitySetting.value = 50 //default
-  val maxQualitySetting = IntRangeSetting("Maximum quality", 1, 100)
-  maxQualitySetting.value = 100 //default
-  val stepSizeSetting = IntRangeSetting("Step size", 1, 15)
-  stepSizeSetting.value = 5 //default
+  val minQualitySetting = FloatRangeSetting("Minimum quality", 0, 1, 100)
+  minQualitySetting.value = 0.5f //default
+  val maxQualitySetting = FloatRangeSetting("Maximum quality", 0, 1, 100)
+  maxQualitySetting.value = 1.0f //default
+  val stepSizeSetting = FloatRangeSetting("Step size", 0, 1, 100)
+  stepSizeSetting.value = 0.05f //default
 
   val settings = List(minQualitySetting, maxQualitySetting, stepSizeSetting)
 
-  private def qmin : Int = minQualitySetting.value
-  private def qmax : Int = maxQualitySetting.value
-  private def step : Int = stepSizeSetting.value
+  private def qmin = minQualitySetting.value
+  private def qmax = maxQualitySetting.value
+  private def qstep = stepSizeSetting.value
 
-  private def introduceJPEGArtifacts(in: BufferedImage, q: Int) = {
+  private def introduceJPEGArtifacts(in: BufferedImage, q: Float) = {
     status("Generating JPEG artifacts")
     val writers = ImageIO.getImageWritersByFormatName("jpeg")
 
@@ -49,7 +49,7 @@ class AnalyserImplementation extends Analyser[ImageMatrix] with Settings {
 
         val param = writer.getDefaultWriteParam
         param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT)
-        param.setCompressionQuality((q.toFloat) / 100f)
+        param.setCompressionQuality(q)
 
         writer.write(null, new IIOImage(rgb, null, null), param)
 
@@ -76,62 +76,28 @@ class AnalyserImplementation extends Analyser[ImageMatrix] with Settings {
     }
   }
 
-  def createOutputImages(in: BufferedImage) : Array[BufferedImage] = {
-    var x = qmin
-    var counter = 0
-    var output = new Array[BufferedImage](ceil((qmax - qmin) / step).toInt + 1)
-    while (x <= qmax)
-    {
-      output(counter) = computeDifference(in, introduceJPEGArtifacts(in, x))
-      counter += 1
-      x += step
-    }
-    output
-  }
-  
-  def computeDifference(in: BufferedImage, compare: BufferedImage): BufferedImage = {
-    val w = in.getWidth
-    val h = in.getHeight
-    var result = new BufferedImage(w,h, BufferedImage.TYPE_INT_ARGB)
-    val graphics = result.getGraphics
-    graphics.setColor(java.awt.Color.BLACK)
-    graphics.clearRect(0,0,w,h)
-    for{
-      x : Int <- 0 until w
-      y : Int <- 0 until h
-    }{
-      val pixelA = in.getRGB(x, y)
-      val pixelB = compare.getRGB(x, y)
+  def range(start: Float, end: Float, step: Float) =
+    for(i <- 0 until ((end - start) / step).toInt) yield start + step * i
 
-      val rA = (pixelA >>> 16)& 255
-      val gA = (pixelA >>> 8) & 255
-      val bA =  pixelA & 255
-      val rB = (pixelB >>> 16)& 255
-      val gB = (pixelB >>> 8) & 255
-      val bB =  pixelB & 255
+  val inputAsBufferedImage = for(in <- input) yield MatrixToImage()(in)
 
-      val diffsq = ((1 / 3) * (pow(rA - rB, 2) + pow(gA - gB, 2) + pow(bA - bB, 2)))
-      result.setRGB(x, y, ((65536 * diffsq) + (256 * diffsq) + diffsq).toInt)
-    }
-    result
+  val outputImages = for(in <- inputAsBufferedImage) yield (for(q <- range(qmin, qmax, qstep)) yield introduceJPEGArtifacts(in, q))
+
+  val outputImagesAsColorMatrices = for(in <- outputImages) yield in map ImageToMatrix()
+
+  val computeDifferences = for(in <- input; imgs <- outputImagesAsColorMatrices) yield
+    (for(img <- imgs) yield img zip in map (imgPair => abs(imgPair._1.weight - imgPair._2.weight)))
+
+  val makeHDR = for(in <- computeDifferences) yield
+    (for(matrix <- in) yield {
+      val max = matrix.max
+      matrix map (x => Color(1, x / max, x / max, x / max))
+    })
+
+  val createReport = for(in <- makeHDR) yield {
+    val imgsWithQuality = in zip range(qmin, qmax, qstep)
+    (for((img, quality) <- imgsWithQuality) yield new ReportImage(Information, "Output at quality " + (quality * 100).toInt, 0, 0, img)).toSet[ReportEntry]
   }
 
-  def ImageToColorMatrix(img: BufferedImage) = new Matrix[Color] {
-    val width = img.getWidth
-    val height = img.getHeight
-    private val raster = img.getData
-    private val buffer = new Array[Int](4)
-    def apply(x: Int, y: Int) = synchronized {
-      raster.getPixel(x, y, buffer)
-      new Color(buffer(3) / 255f, buffer(0) / 255f, buffer(1) / 255f, buffer(2) / 255f)
-    }
-  }
-
-  def createReport(in: Array[BufferedImage]) : Set[ReportEntry] = {
-    (for{ i : Int <- 0 until in.length} yield {
-        new ReportImage(Information, "Output at quality " + (qmin + i * step).toString(), 0, 0, ImageToColorMatrix(in(i)))
-      }).toSet[ReportEntry]
-  } 
-
-  val result = input >- MatrixToImage() >- createOutputImages >- createReport
+  val result = createReport
 }
