71112ed9468f8610da72e71b446b4f4acac238f5
mroth23 <moritz.gemini92@telia.com>
2010-05-17 22:28:53 +0200

    DCT update

--- purview-core/src/main/scala/org/purview/core/transforms/DiscreteCosineTransform.scala
+++ purview-core/src/main/scala/org/purview/core/transforms/DiscreteCosineTransform.scala
@@ -6,27 +6,30 @@ import scala.math._
 case class DiscreteCosineTransform() extends Function1[Matrix[Float], Matrix[Float]] {
 
   def coefficients(bw: Int, bh: Int) : Matrix[Float] = new Matrix[Float] {
-      val width = bw
-      val height = bh
+    val width = bw
+    val height = bh
 
-      private val Double = (sqrt(1f / bw) * sqrt(1f / bh)).toFloat
-      private val Sans = (sqrt(2f / bw) * sqrt(2f / bh )).toFloat
-      private val Single = (sqrt(Double) * sqrt(Sans)).toFloat
+    private val Double = (sqrt(1f / bw) * sqrt(1f / bh)).toFloat
+    private val Sans = (sqrt(2f / bw) * sqrt(2f / bh )).toFloat
+    private val SingleX = (sqrt(1f / bw) * sqrt(2f / bh)).toFloat
+    private val SingleY = (sqrt(1f / bh) * sqrt(2f / bw)).toFloat
 
-      //Don't actually store the matrix; select values on the fly
-      def apply(x: Int, y: Int) = if(x == 0 && y == 0)
-        Double
-      else if(x == 1 || y == 1)
-        Single
-      else
-        Sans
-    }
+    //Don't actually store the matrix; select values on the fly
+    def apply(x: Int, y: Int) = if(x == 0 && y == 0)
+      Double
+    else if(x == 0 && y != 0)
+      SingleX
+    else if(y == 0 && x != 0)
+      SingleY
+    else
+      Sans
+  }
 
   def apply(data: Matrix[Float]): Matrix[Float] = {
     val w = data.width
     val h = data.height
     val result = new MutableArrayMatrix[Float](w, h)
-    val pi = 3.141592653589793f
+    val pi = 3.141592653589793238463f
     val coeff = coefficients(w, h)
     var v = 0
     while(v < w) {
@@ -58,46 +61,47 @@ case class DiscreteCosineTransform() extends Function1[Matrix[Float], Matrix[Flo
 case class InverseDiscreteCosineTransform() extends Function1[Matrix[Float], Matrix[Float]] {
 
   def coefficients(bw: Int, bh: Int) : Matrix[Float] = new Matrix[Float] {
-      val width = bw
-      val height = bh
+    val width = bw
+    val height = bh
 
-      private val Double = 1f / (sqrt(1f / bw) * sqrt(1f / bh)).toFloat
-      private val Sans = 1f / (sqrt(2f / bw) * sqrt(2f / bh )).toFloat
-      private val Single = 1f / (sqrt(Double) * sqrt(Sans)).toFloat
+    private val Float = (sqrt(1f / bw) * sqrt(1f / bh)).toFloat
+    private val Sans = (sqrt(2f / bw) * sqrt(2f / bh )).toFloat
+    private val SingleX = (sqrt(1f / bw) * sqrt(2f / bh)).toFloat
+    private val SingleY = (sqrt(1f / bh) * sqrt(2f / bw)).toFloat
 
-      //Don't actually store the matrix; select values on the fly
-      def apply(x: Int, y: Int) = if(x == 0 && y == 0)
-        Double
-      else if(x == 1 || y == 1)
-        Single
-      else
-        Sans
-    }
+    //Don't actually store the matrix; select values on the fly
+    def apply(x: Int, y: Int) = if(x == 0 && y == 0)
+      Float
+    else if(x == 0 && y != 0)
+      SingleX
+    else if(y == 0 && x != 0)
+      SingleY
+    else
+      Sans
+  }
 
   def apply(data: Matrix[Float]): Matrix[Float] = {
     val w = data.width
     val h = data.height
     val result = new MutableArrayMatrix[Float](w, h)
-    val pi = 3.141592653589793f
+    val pi = 3.141592653589793238463f
     val coeff = coefficients(w, h)
-    val dc = (1f / 2f) * data(0, 0)
     var v = 0
     while(v < w) {
       var u = 0
       while(u < h) {
 
-        var sum = dc
+        var sum = 0f
         //Actually calculate the local sum
         var y = 0
         while(y < h) {
           var x = 0
           while(x < w) {
-            sum += (data(x, y) * cos((pi / w) * x * (v - 1f / 2f)) * cos((pi / h) * y * (u - 1f / 2f))).toFloat
+            sum += (coeff(x, y) * data(x, y) * (cos(pi * u * (2.0 * x + 1.0) / (w * 2)) * cos(pi * v * (2.0 * y + 1.0) / (h * 2)))).toFloat
             x += 1
           }
           y += 1
         }
-        sum *= coeff(u, v)
         result(u, v) = sum
         u += 1
       }
--- purview-core/src/test/scala/org/purview/core/transforms/DCTSpec.scala
+++ purview-core/src/test/scala/org/purview/core/transforms/DCTSpec.scala
@@ -6,7 +6,14 @@ import org.specs.SpecificationWithJUnit
 class DCTSpec extends SpecificationWithJUnit {
   "A discrete cosine transform" should {
     "be reversible" in {
-      val data = ImmutableMatrix(4,4, Array.fill(16)(util.Random.nextInt(255) - 128f))
+      val data = ImmutableMatrix(8,8, Array(-76d,-73d,-67d,-62d,-58d,-67d,-64d,-55d,
+                                            -65d,-69d,-73d,-38d,-19d,-43d,-59d,-56d,
+                                            -66d,-69d,-60d,-15d, 16d,-24d,-62d,-55d,
+                                            -65d,-70d,-57d, -6d, 26d,-22d,-58d,-59d,
+                                            -61d,-67d,-60d,-24d, -2d,-40d,-60d,-58d,
+                                            -49d,-63d,-68d,-58d,-51d,-60d,-70d,-53d,
+                                            -43d,-57d,-64d,-69d,-73d,-67d,-63d,-45d,
+                                            -41d,-49d,-59d,-60d,-63d,-52d,-50d,-34d))
       val dct  = DiscreteCosineTransform()
       val idct = InverseDiscreteCosineTransform()
       val reconstructed = idct(dct(data))
